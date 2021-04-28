347730b5f6f52e1ad9f3ee3f8d3c1eefdd735b53
Rokas <rookas.rudzenskas@gmail.com>
2021-04-28 18:17:56 +0300

    Rokas

--- my_app/views.py
+++ my_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "base.html", {})
+    return render(request, "my_app/new_searches.html", {})
